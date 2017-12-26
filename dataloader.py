6edfe352aa28e25aa11e4e8ce21c85b40a2b20cb
Leo Mao <r06921053@ntu.edu.tw>
2017-12-26 21:21:54 +0800

    Add dataloader for normal CBOW method (without syntax)

--- dataloader.py
+++ dataloader.py
@@ -1,172 +1,131 @@
-import re
 import numpy as np
-from collections import deque
+import torch
 from torch.utils.data import Dataset, DataLoader
 
-from nltk.parse import corenlp
+from utils import AliasTable, sliding_window, np2tor, sum_normalize
 
 
-MAX_CHILDREN = 5
+NEG_SAMPLES = 5
 
 
-WORD_TYPE = [
-    ('word_idx', 'i4'),
-    ('pos_idx', 'u1'),
-    ('pr_pos_idx', 'u1'),
-    ('pr_dep_idx', 'u1'),
-    ('ch_pos_idxs', f'{MAX_CHILDREN}u1'),
-    ('ch_dep_idxs', f'{MAX_CHILDREN}u1'),
-]
-
-
-def sliding_window(arr, size):
-    '''
-        Create a sliding window view of the original array.
-        This function has to be used with extreme care!
-    '''
-    axis = 0
-    shape = list(arr.shape)
-    shape[axis] = arr.shape[axis] - size + 1
-    shape.append(size)
-    strides = list(arr.strides)
-    strides.append(arr.strides[axis])
-    return np.lib.stride_tricks.as_strided(
-        arr, shape=shape, strides=strides, writeable=False
-    )
-
-
-def np2tor(arr):
-    return torch.from_numpy(arr)
-
-
-class SentenceParsingDataset(Dataset):
-    def __init__(self, filepath, dep_parser, vocab_map, pos_map, dep_map):
+class SentenceDataset(Dataset):
+    def __init__(self, filepath, vocab_map):
         '''
             filepath: path to the corpus (one line per sentence)
-            dep_parser: corenlp.CoreNLPDependencyParser
             vocab_map: LookupTable
-            pos_map: LookupTable
-            dep_map: LookupTable
-            
-            Note:
-                `0` is treated as padding index.
-                LookupTable should map OOV to the `unknown` index.
         '''
-        self.dep_parser = 
-        self.dep_parser = dep_parser
         self.vocab_map = vocab_map
-        self.pos_map = pos_map
-        self.dep_map = dep_map
         with open(filepath, 'r') as f:
-            self.sents = [line.strip() for line in f]
-        self.cache = {}
+            tokens = line.strip().split()
+            self.sents = [
+                np.array([self.vocab_map[t] for t in tokens], dtype='i')
+                for line in f
+            ]
 
     def __getitem__(self, index):
-        if index in self.cache:
-            return self.cache[index]
-        else:
-            self.cache[index] = self.parse(self.sents[index])
-            return self.cache[index]
+        return self.sents[index]
 
     def __len__(self):
         return len(self.sents)
 
-    def parse(self, sentence):
-        nodes = next(self.dep_parser.raw_parse(sentence)).nodes
-        word_n = len(nodes) - 1
-        sent = np.zeros(word_n, dtype=WORD_TYPE)
-        for i in range(word_n):
-            node = nodes[i+1]
-            word = sent[i]
-
-            word['word_idx'] = self.vocab_map[node['word']]
-            word['pos_idx'] = self.pos_map[node['tag']]
-            word['pr_pos_idx'] = self.pos_map[nodes[node['head']]['tag']]
-            word['pr_dep_idx'] = self.dep_map[node['rel']]
-            ch = []
-            for dep, arr in node['deps'].items():
-                ch.extend([(nodes[x]['tag'], dep) for x in arr])
-
-            for j, (pos, dep) in enumerate(ch[:MAX_CHILDREN]):
-                word['ch_pos_idxs'][j] = self.pos_map[pos]
-                word['ch_dep_idxs'][j] = self.dep_map[dep]
-
-        return sent
-
 
 class CBOWLoaderIter:
     def __init__(self, loader):
-        self.loader = laoder
-        self.shuffle = laoder.shuffle
+        self.loader = loader
+        self.shuffle = loader.shuffle
         self.batch_size = loader.batch_size
         self.window_size = loader.window_size
+        self.padding_index = loader.padding_index
+
+        neg_prob = sum_normalize(idx_count ** loader.neg_power)
+        self.neg_table = AliasTable(neg_prob)
+        ratio = loader.sub_threshold / sum_normalize(loader.idx_count)
+        self.sub_prob = np.sqrt(ratio) + ratio
+
         self.in_iter = iter(loader.in_loader)
         self.queue = []
 
     def get_next_sent(self):
-        try:
-            sent = next(self.in_iter)
-        except StopIteration:
-            return None, None
-        word_n = len(sent)
-        word_idx = sent['word_idx']
+        if self.loader.cached:
+            return next(self.cache_iter)
+        else:
+            sent = next(self.in_iter)[0]
+            self.loader.cache.append(sent)
+            return sent
+
+    def gen_context(self, sent):
         size = self.window_size
         # pad zeros
-        pad_word_idx = np.pad(word_idx, (size, size))
+        pad_word_idx = np.pad(sent, (size, size), 'constant',
+                              constant_values=self.padding_index)
         # following is correct but not easy to understand
         wds = sliding_window(pad_word_idx, size)
         ctx = np.concatenate((wds[:-(size+1)], wds[(size+1):]), axis=1)
-        return sent, ctx
+        return ctx
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        total = sum(len(x) for x, y in self.queue)
-        while total < self.batch_size:
-            sent, ctx = self.get_next_sent()
-            if sent is None:
+        n_sample = sum(len(x) for x, y in self.queue)
+        while n_sample < self.batch_size:
+            try:
+                # get the next sentence, use cache if available
+                sent = self.get_next_sent()
+            except StopIteration:
                 break
-            # TODO:
+
             # subsampling
-            total += len(sent)
+            mask = self.sub_prob[sent] > np.random.random_sample(len(sent))
+            sent = sent[mask]
+            if len(sent) < 2:
+                continue
+            # generate context
+            ctx = self.gen_context(sent)
+            n_sample += len(sent)
             self.queue.append((sent, ctx))
 
-        if total == 0:
+        if n_sample == 0:
+            # end of iteration, set cached to True
+            self.loader.cached = True
             raise StopIteration
 
-        bound = total - self.batch_size
+        bound = n_sample - self.batch_size
         remaining = None
         if bound > 0:
+            n_sample = self.batch_size
             last_sent, last_ctx = self.queue.pop()
             remaining = (last_sent[-bound:], last_ctx[-bound:])
-            self.queue.append((last_sent[-bound:], last_ctx[-bound:]))
+            self.queue.append((last_sent[:-bound], last_ctx[:-bound]))
 
         sent, ctx = [np.concatenate(arr) for arr in zip(*self.queue)]
 
         self.queue.clear()
-        if remaining is not None;
+        if remaining is not None:
             self.queue.append(remaining)
 
-        word_idx = np2tor(sent['word_idx']).long()
-        pos_idx = np2tor(sent['pos_idx'])).long()
-        pr_pos_idx = np2tor(sent['pr_pos_idx']).long()
-        pr_dep_idx = np2tor(sent['pr_dep_idx']).long()
-        ch_pos_idxs = np2tor(sent['ch_pos_idxs']).long()
-        ch_dep_idxs = np2tor(sent['ch_dep_idxs']).long()
-        ctx_idxs = np2tor(ctx).long()
-        return (word_idx, pos_idx,
-                pr_pos_idx, pr_dep_idx,
-                ch_pos_idxs, ch_dep_idxs,
-                ctx_idxs)
+        word_idx = np2tor(sent)
+        ctx_idxs = np2tor(ctx)
+        neg_idxs = np2tor(self.neg_table.sample(n_sample, NEG_SAMPLES))
+        return (word_idx, ctx_idxs, neg_idxs)
 
 
 class CBOWLoader:
-    def __init__(self, dataset, window_size,
-                 batch_size=1, shuffle=False,
-                 num_workers=0):
-        self.window_size,
+    def __init__(self, dataset, window_size, idx_count, padding_index,
+                 neg_power=0.75, sub_threshold=1e-5,
+                 batch_size=1, shuffle=False, num_workers=0):
+        '''
+            window_size: int
+            idx_count: numpy array (1D)
+        '''
         self.dataset = dataset
+        self.window_size = window_size
+        self.idx_count = idx_count
+        self.padding_index = padding_index
+        self.neg_power = neg_power
+        self.sub_threshold = sub_threshold
+        self.shuffle = shuffle
+        self.batch_size = batch_size
         self.in_loader = DataLoader(dataset, batch_size=1, shuffle=shuffle,
                                     num_workers=num_workers,
                                     collate_fn=lambda x: x)
